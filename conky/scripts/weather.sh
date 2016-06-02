dbaa36b41adedcd6c512e9a0c01891cb732fdae8
Jason Joyce <jjoyce@redhat.com>
2016-06-02 07:37:39 -0400

    Changing conky weather layout.

--- conky/scripts/weather.sh
+++ conky/scripts/weather.sh
@@ -14,4 +14,4 @@ if [ -z $1 ]; then
 	exit 0;
 fi
 
-curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }'
+curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "\n\t$1"; }'
