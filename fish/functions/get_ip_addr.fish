6d6ddfd7127c001712cf08b5d64755964efa22d0
Jason Joyce <fuzzball81@gmail.com>
2016-07-24 14:12:07 -0400

    Adding better ip support for fish shell prompt.

--- fish/functions/get_ip_addr.fish
+++ fish/functions/get_ip_addr.fish
@@ -1,8 +1,16 @@
 function get_ip_addr --description 'Print the current IP addesss'
-	set -l interfaces tun0 eth0 enp0s25 wlp3s0
-	for val in $interfaces
-		set ip (ip address show dev $val | grep inet | grep -v inet6 | awk -F " " '{print $2}' | awk -F "/" '{print $1}')
-		break
+	switch (uname)
+		case Linux
+			set -l interfaces tun0 eth0 enp0s25 wlp3s0
+			for val in $interfaces
+				set ip (ip address show dev $val | grep inet | grep -v inet6 | awk -F " " '{print $2}' | awk -F "/" '{print $1}')
+				break
+			end
+
+		case Darwin
+			set ip (ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{ print $2 }')
+		case '*'
+			set ip "Can't get IP"
 	end
 
 	echo "$ip"
