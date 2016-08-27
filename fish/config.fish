da1756c59868c0add7eed82eea10e180522e6d30
Jason Joyce <fuzzball81@gmail.com>
2016-08-27 12:17:41 -0400

    Adding extra paths for MacOS in fish/config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,9 @@ set -gx PATH $PATH /usr/bin
 switch (uname)
 	case Linux
 		set -gx PATH $PATH /usr/local/sbin
+
+	case Darwin
+		set -gx PATH $PATH {$HOME}/Library/Python/2.7/bin
 end
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
