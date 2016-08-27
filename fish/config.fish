ce1cbdac71515c7e6722eee3e3fde58163e8c4b3
Jason Joyce <fuzzball81@gmail.com>
2016-08-27 12:26:44 -0400

    Adding /usr/local/bin to fish PATH.

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ switch (uname)
 		set -gx PATH $PATH /usr/local/sbin
 
 	case Darwin
-		set -gx PATH $PATH {$HOME}/Library/Python/2.7/bin
+		set -gx PATH $PATH {$HOME}/Library/Python/2.7/bin /usr/local/bin
 end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
